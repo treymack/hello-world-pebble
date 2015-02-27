9a63dd2672e198cd8dd9b9051cdeced5cc66a9ed
Trey Mack <tmack10@gmail.com>
2015-02-26 23:39:57 -0500

    refresh + vibe

--- src/app.js
+++ src/app.js
@@ -1,38 +1,49 @@
 var UI = require('ui');
 var ajax = require('ajax');
+var vibe = require('ui/vibe');
 
 var card = new UI.Card({
-  title: 'Weather',
-  subtitle: 'Fetching ...'
+  title: 'Weather'
 });
 
 card.show();
 
-var cityName = 'Lexington,SC';
-var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+function refresh() {
+  card.subtitle('Fetching...');
+  card.body('\n\n' + 'tmack rocks');
 
-ajax(
-{
-  url: url,
-  type: 'json'
-},
-function(data) {
-  console.log('success');
-  console.log(data);
-  
-  var location = data.name;
-  var k = data.main.temp;
-  var f = (k - 273.15) * 1.8 + 32;
-  var temp = Math.round(f) + 'F';
-  var desc = data.weather[0].description + '\n' + '\n' + 'tmack rocks';
-  
-  card.subtitle(location + ', ' + temp);
-  card.body(desc);
-},
-function(error) {
-  console.log('error');
-  console.log(error);
-  
-  card.subtitle('Error!');
-  card.body(error);
-});
+  var cityName = 'Lexington,SC';
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+
+  ajax(
+  {
+    url: url,
+    type: 'json'
+  },
+  function(data) {
+    console.log('success');
+    console.log(data);
+
+    var location = data.name;
+    var k = data.main.temp;
+    var f = (k - 273.15) * 1.8 + 32;
+    var temp = Math.round(f) + 'F';
+    var desc = data.weather[0].description + '\n' + '\n' + 'tmack rocks';
+
+    card.subtitle(location + ', ' + temp);
+    card.body(desc);
+    vibe.vibrate('short');
+  },
+  function(error) {
+    console.log('error');
+    console.log(error);
+
+    card.subtitle('Error!');
+    card.body(error);
+    vibe.vibrate('double');
+  });
+}
+
+refresh();
+
+card.on('click', refresh);
